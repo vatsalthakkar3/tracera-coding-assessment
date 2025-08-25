cce6ecc68f0282e8c45766a348b21c890cf88829
vatsalthakkar3 <vatsalthakkar3.vt@gmail.com>
2025-08-24 18:14:06 -0700

    feat(): Adding test coverage adn precommit

---
+++ src/dummy.py
@@ -0,0 +1,17 @@
+"""
+core.py
+
+This module provides basic arithmetic operations.
+"""
+
+
+def add(a: int, b: int) -> int:
+    """
+    Adds two integers and returns their sum.
+    Args:
+        a (int): The first integer to add.
+        b (int): The second integer to add.
+    Returns:
+        int: The sum of a and b.
+    """
+    return a + b
